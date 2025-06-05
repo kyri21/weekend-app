3f503994bdd7ccf79ec7311f3b16f10795b96f63
kyri21 <kyriarthur@gmail.com>
2025-06-05 21:20:17 +0200

    Update Lieux.py

--- pages/Lieux.py
+++ pages/Lieux.py
@@ -9,6 +9,7 @@ from streamlit_folium import st_folium
 # Initialisation Firebase
 if "firebase_initialized" not in st.session_state:
     cred = credentials.Certificate("firebase_key.json")
+    if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
     st.session_state.firebase_initialized = True
 
