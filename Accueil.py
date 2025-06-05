a344a44c93527e0e13e7af01f12344a6e099b6a6
kyri21 <kyriarthur@gmail.com>
2025-06-05 19:53:53 +0200

    Add files via upload

---
+++ Accueil.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="App à WAF", layout="wide")
+
+st.title("🏕️ App à WAF")
+st.markdown("""
+Bonsoir à tous c'est Oson Welles ! Utilise le menu à gauche pour :
+- Saisir tes disponibilités
+- Consulter la trésorerie et les dépenses de chacun des bureaux les années précédentes
+- Consulter les infos utiles
+- Voir les lieux déjà visités
+- Consulter les bureaux annuels
+""")
