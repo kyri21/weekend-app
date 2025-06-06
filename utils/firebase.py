4b338c7b7985494b6130a7217321ea004033f5fa
kyri21 <kyriarthur@gmail.com>
2025-06-06 11:06:36 +0200

    Update firebase.py

--- utils/firebase.py
+++ utils/firebase.py
@@ -5,25 +5,26 @@ import json
 
 # Initialise Firebase une seule fois
 if not firebase_admin._apps:
-    # Récupère le secret
+    # Récupère le secret défini dans Streamlit Cloud
     secret = st.secrets.get("FIREBASE_KEY")
 
-    # Si c'est une chaîne (JSON brut), on décode en dict
+    # Si le secret est une chaîne (JSON brut), on la convertit en dict
     if isinstance(secret, str):
         try:
             firebase_key_dict = json.loads(secret)
         except json.JSONDecodeError as e:
-            st.error("Erreur JSON dans ton secret FIREBASE_KEY : " + str(e))
+            st.error("Erreur JSON dans le secret FIREBASE_KEY : " + str(e))
             st.stop()
-    # Sinon, s’il s'agit déjà d'un dict (format TOML table), on l'utilise tel quel
+    # Si le secret est déjà un dict (format TOML table), on l’utilise directement
     elif isinstance(secret, dict):
         firebase_key_dict = secret
     else:
         st.error(f"Type inattendu pour st.secrets['FIREBASE_KEY'] : {type(secret)}")
         st.stop()
 
-    # Initialisation
+    # Initialise l’app Firebase avec ce dict
     cred = credentials.Certificate(firebase_key_dict)
     firebase_admin.initialize_app(cred)
 
+# Obtient le client Firestore
 db = firestore.client()
