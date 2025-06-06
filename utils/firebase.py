1212ac4fe598c6b25895f621569cf6cc3ab1cac5
kyri21 <kyriarthur@gmail.com>
2025-06-06 10:34:53 +0200

    Update firebase.py

--- utils/firebase.py
+++ utils/firebase.py
@@ -1,10 +1,12 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
+import json
 
-# Initialisation Firebase une seule fois
+# Initialise Firebase **UNE SEULE FOIS** en lisant st.secrets
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    firebase_key_dict = json.loads(st.secrets["FIREBASE_KEY"])
+    cred = credentials.Certificate(firebase_key_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
